768d02b1098cf4ff74bca74a58d4c172fbf62897
michalrudy9 <michallrudy9@gmail.com>
2024-02-25 11:33:53 +0100

    Added loading info when fetching data

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,10 +2,15 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
 
 export default function MealsPage() {
-  const meals = getMeals();
-
   return (
     <>
       <header className={classes.header}>
@@ -21,7 +26,11 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching melas...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
